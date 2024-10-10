823fc4159201c0a51f76eb4408ab0de27a9574df
Захаров Денис <zdv.work@yandex.ru>
2024-10-10 20:05:00 +0600

    user model

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -2,3 +2,7 @@ export {
   UserEntity,
   UserModel
 } from './user.entity.js';
+
+export { CreateUserDto } from './dto/create-user.dto.js';
+export { DefaultUserService } from './default-user.service.js';
+export { createUserContainer } from './user.container.js';
