1fb33bc2ab890a7e51abe953d9f3207c58b3a777
Захаров Денис <zdv.work@yandex.ru>
2024-11-02 11:28:47 +0600

    cors

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -10,6 +10,7 @@ import { Controller } from '../shared/libs/rest/index.js';
 import { ExceptionFilter } from '../shared/libs/rest/exception-filter/exception-filter.interface.js';
 import { ParseTokenMiddleware } from '../shared/libs/rest/middleware/parse-token.middleware.js';
 import { STATIC_FILES_ROUTE, STATIC_UPLOAD_ROUTE } from './index.js';
+import cors from 'cors';
 
 @injectable()
 export class RestApplication {
@@ -66,6 +67,7 @@ export class RestApplication {
       express.static(this.config.get('STATIC_DIRECTORY_PATH'))
     );
     this.server.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
+    this.server.use(cors());
   }
 
   private async _initExceptionFilters() {
