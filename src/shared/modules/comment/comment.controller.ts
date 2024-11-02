9607310050dca689e4f614f13ed9e022c7f50954
Захаров Денис <zdv.work@yandex.ru>
2024-11-02 23:24:44 +0600

    добавил проверку

--- src/shared/modules/comment/comment.controller.ts
+++ src/shared/modules/comment/comment.controller.ts
@@ -1,11 +1,12 @@
 import { inject, injectable } from 'inversify';
-import { BaseController, HttpMethod, PrivateRouteMiddleware, ValidateDtoMiddleware } from '../../libs/rest/index.js';
+import { BaseController, HttpError, HttpMethod, PrivateRouteMiddleware, ValidateDtoMiddleware } from '../../libs/rest/index.js';
 import { Component } from '../../types/component.enum.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
 import { CommentService } from './comment-service.interface.js';
 import { CreateCommentDto } from './dto/create-comment.dto.js';
 import { Request, Response } from 'express';
 import { OfferService } from '../offer/offer-service.interface.js';
+import { StatusCodes } from 'http-status-codes';
 
 @injectable()
 export class CommentController extends BaseController {
@@ -32,8 +33,18 @@ export class CommentController extends BaseController {
     {body, tokenPayload}: Request<Record<string, unknown>, Record<string, unknown>, CreateCommentDto>,
     res: Response
   ): Promise<void> {
+    const { offerId } = body;
+    const offer = await this.offerService.exists(offerId);
+    if (!offer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id=${offerId} not found.`,
+        'DocumentExistsMiddleware'
+      );
+    }
+
     const result = await this.commentService.create({...body, userId: tokenPayload.id});
-    await this.offerService.incCommentCount(body.offerId);
+    await this.offerService.incCommentCount(offerId);
     this.created(res, result);
   }
 }
